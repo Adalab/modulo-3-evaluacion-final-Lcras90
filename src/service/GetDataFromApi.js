aba01badb89154111a06fa210fd1da6c8985860a
Iván (profe) <ivan@adalab.es>
2021-06-28 16:57:40 +0200

    Implement pagination. 1st version.

--- src/service/GetDataFromApi.js
+++ src/service/GetDataFromApi.js
@@ -1,20 +1,35 @@
+const ENDPOINT = "https://rickandmortyapi.com/api/character";
+const charactersList = [];
+
+function fetchPage(response)  {
+  const charactersOfThisPage = response.results.map((character) => {
+    return {
+      id: character.id,
+      name: character.name,
+      status: character.status,
+      species: character.species,
+      image: character.image,
+      episode: character.episode.length,
+      origin: character.origin.name,
+    };
+  });
+  charactersList.push( ...charactersOfThisPage );
+
+  if( response.info.next !== null ) {
+    return fetch(response.info.next)
+    .then((response) => response.json())
+    .then( fetchPage );
+  }
+  else {
+    return charactersList;
+  }
+  
+}
+
 function GetDataFromApi() {
-  const ENDPOINT = "https://rickandmortyapi.com/api/character";
   return fetch(ENDPOINT)
     .then((response) => response.json())
-    .then((response) =>
-      response.results.map((character) => {
-        return {
-          id: character.id,
-          name: character.name,
-          status: character.status,
-          species: character.species,
-          image: character.image,
-          episode: character.episode.length,
-          origin: character.origin.name,
-        };
-      })
-    );
+    .then( fetchPage );
 }
 function GetDataFromApibyName(name) {
   const ENDPOINT = "https://rickandmortyapi.com/api/character/?name=";
